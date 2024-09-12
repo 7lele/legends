c2fa79b76eee22f7eea6a725f33fe2a72983c2d3
7lele <118902819+7lele@users.noreply.github.com>
2024-09-12 19:22:18 +0200

    #0006

--- main.lua
+++ main.lua
@@ -7,29 +7,29 @@ function love.load()
 
     
     map = love.graphics.newImage("Assets/map.png")
-
+    map:setFilter("nearest", "nearest")
     -- player 
     screenWidth = love.graphics.getWidth()
     screenHeight = love.graphics.getHeight()
     player = love.graphics.newImage("Assets/player.png")
     imageX = (screenWidth - 16) / 2
     imageY = (screenHeight - 16) / 2
-
+    
 end
 
 
 function love.update()
     if love.keyboard.isDown("w")== true then
-        Y = Y + 16
+        Y = Y + 6
     end
     if love.keyboard.isDown("s")== true then
-        Y = Y - 16
+        Y = Y - 6
     end
     if love.keyboard.isDown("a")== true then
-        X = X + 16
+        X = X + 6
     end
     if love.keyboard.isDown("d")== true then
-        X = X - 16
+        X = X - 6
     end
         
 end
@@ -37,8 +37,8 @@ end
 
 function love.draw()
 
-    love.graphics.draw(map, X/16, Y/16)
+    love.graphics.draw(map, X, Y,0,3)
     love.graphics.draw(player, imageX, imageY)
-    love.graphics.print(X,screenWidth - 100, 9, 0, 1.2)
-    love.graphics.print(Y,screenWidth - 45, 9, 0, 1.2)
+    love.graphics.print(X/48,screenWidth - 100, 9, 0, 1.2)
+    love.graphics.print(Y/48,screenWidth - 45, 9, 0, 1.2)
 end
