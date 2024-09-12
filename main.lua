be482a84a30b28735b7352d74ed63fdbb6bd32be
7lele <118902819+7lele@users.noreply.github.com>
2024-09-12 18:50:34 +0200

    #0004

--- main.lua
+++ main.lua
@@ -2,7 +2,15 @@ _G.love = require("love")
 
 
 function love.load()
-    
+    map = love.graphics.newImage("Assets/map.png")
+
+    --player 
+    screenWidth = love.graphics.getWidth()
+    screenHeight = love.graphics.getHeight()
+    player = love.graphics.newImage("Assets/player.png")
+    imageX = (screenWidth - 16) / 2
+    imageY = (screenHeight - 16) / 2
+
 end
 
 
@@ -12,5 +20,6 @@ end
 
 
 function love.draw()
-
+    love.graphics.draw(map, 0, 0)
+    love.graphics.draw(player, imageX, imageY)
 end
