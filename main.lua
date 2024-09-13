b2fe6feb7b0045d47169394576f9c9c4624838dd
7lele <118902819+7lele@users.noreply.github.com>
2024-09-13 13:49:07 +0200

    #0007

--- main.lua
+++ main.lua
@@ -19,26 +19,33 @@ end
 
 
 function love.update()
+    xrel = math.floor(X/48 * -1)
+    yrel = math.floor(Y/48 * -1)
     if love.keyboard.isDown("w")== true then
-        Y = Y + 6
+        Y = Y + 3
     end
     if love.keyboard.isDown("s")== true then
-        Y = Y - 6
+        Y = Y - 3
     end
     if love.keyboard.isDown("a")== true then
-        X = X + 6
+        X = X + 3
     end
     if love.keyboard.isDown("d")== true then
-        X = X - 6
+        X = X - 3
     end
         
+    if X > 390 then
+        X = 390
+    end 
 end
 
 
 function love.draw()
-
-    love.graphics.draw(map, X, Y,0,3)
+    
+    love.graphics.draw(map, math.floor(X), math.floor(Y),0,3)
     love.graphics.draw(player, imageX, imageY)
-    love.graphics.print(X/48,screenWidth - 100, 9, 0, 1.2)
-    love.graphics.print(Y/48,screenWidth - 45, 9, 0, 1.2)
+    love.graphics.print(math.floor(X/48 * -1),screenWidth - 100, 9, 0, 1.2)
+    love.graphics.print(math.floor(Y/48 * -1),screenWidth - 45, 9, 0, 1.2)
+    love.graphics.print(X,screenWidth - 800, 9, 0, 1.2)
+    love.graphics.print(Y,screenWidth - 765, 9, 0, 1.2)
 end
